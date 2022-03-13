99322522915765a4b5604b3d377cb0461f07d412
Tyler <tylerclancy030@gmail.com>
2022-03-13 14:29:52 -0500

    Add response to default route and listener

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!');
+});
